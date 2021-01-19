e51599921a6546abf57eda16ae298a19d40be894
nolepeople <muhakemos@gmail.com>
2021-01-19 12:38:16 +0000

    -

---
+++ numpy/array-dimension.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+a = {
+        #1 dimension list
+        1:np.array([1,2,3,4,5]),
+        #2 dimension list
+        2:np.array([[1,2,3],[4,5,6]])
+        }
+
+print (f"Array:1:Dimensi\n {a[1]}",end="\n")
+print (f"Array:2:Dimensi\n {a[2]}")
+
+
