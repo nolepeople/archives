6e75dd03320ccda41065387da09697d9dc9ead08
nolepeople <nolepeople@example.com>
2021-01-22 14:58:23 +0000

    -

--- pandas/read-file-format/4-read_html.py
+++ pandas/read-file-format/4-read_html.py
@@ -3,10 +3,8 @@ import requests
 import pandas as pd
 
 head = {"User-Agent":"Mozilla/5.0 (Linux; Android 7.1.2; Redmi 5A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.110 Mobile Safari/537.36"}
-
-url = "https://www.animecharactersdatabase.com/characters.php?id=73892"
-
-src_code = requests.get(url,headers=head)
+src_code = \
+requests.get("https://www.animecharactersdatabase.com/characters.php?id=73892",headers=head)
 def get_table(html_code=pd.read_html(src_code.text)):
     df = pd.DataFrame(html_code[3]).drop(9)
     df = df.rename(columns={0:"key",1:"value"})
