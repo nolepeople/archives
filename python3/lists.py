6f1189217a1c8d094bded9f300af3d4bbabccd78
nolepeople <muhakemos@gmail.com>
2021-02-16 17:40:46 +0200

    -

---
+++ python3/lists.py
@@ -0,0 +1,25 @@
+
+
+List = [1,2,3,4,5]
+
+# length
+print (len(List))
+
+# conca tenation
+a = List + [6,7,8,9,10]
+print (a)
+
+# repetition
+a = List * 3
+print (a)
+
+# membership
+print (2 in List)
+
+# iteration
+for _ in List:
+    print (_,end=" ")
+
+
+
+
